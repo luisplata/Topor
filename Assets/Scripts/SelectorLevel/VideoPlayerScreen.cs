35e6e403fa50ed2803b6fe2a3c6961cea40034e6
Luis Plata <www.luisplata@gmail.com>
2024-06-29 13:36:23 -0500

    adding splash scene

---
+++ Assets/Scripts/SelectorLevel/VideoPlayerScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+public class VideoPlayerScreen : MonoBehaviour
+{
+    public UnityEvent OnFinishVideo;
+    [SerializeField] private VideoPlayer videoPlayer;
+    
+    private void Start()
+    {
+        videoPlayer.Prepare();
+        videoPlayer.prepareCompleted += source =>
+        {
+            videoPlayer.Play();
+            StartCoroutine(FinishVideo());
+        };
+    }
+
+    private IEnumerator FinishVideo()
+    {
+        yield return new WaitForSeconds((float)videoPlayer.length);
+        videoPlayer.Stop();
+        OnFinishVideo.Invoke();
+    }
+}
