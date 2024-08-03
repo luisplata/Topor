d4b35296df472963310df98acb1b5d80fc8e79f2
Luis Plata <www.luisplata@gmail.com>
2024-08-02 21:31:30 -0500

    upload images and config scroll in select level

---
+++ Assets/Scripts/SelectorLevel/EventsSoundController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventsSoundController : MonoBehaviour
+{
+    [SerializeField] private ScrollRect scrollRect;
+    public bool isScrolling;
+    public float velocity;
+
+    private void Update()
+    {
+        if (isScrolling)
+        {
+            velocity = scrollRect.velocity.x;
+            if (velocity is > 0.1f and < 1f)
+            {
+                Debug.Log($"burrrrr");
+            }
+        }
+    }
+
+    public void OnScrollStart()
+    {
+        isScrolling = true;
+    }
+
+    public void OnScrollEnd()
+    {
+        isScrolling = false;
+        Debug.Log($"Release");
+    }
+}
--- Assets/Scripts/SelectorLevel/ScrollMap.cs
+++ Assets/Scripts/SelectorLevel/ScrollMap.cs
@@ -1,20 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class ScrollMap : MonoBehaviour
 {
-    [SerializeField] private ScrollRect scrollRect;
-    private float _maxVelocity = float.MinValue;
-
-
-    public void OnChangeValue(Vector2 floatValue)
-    {
-        var forceToScroll = Mathf.Abs(scrollRect.velocity.x);
-        if (forceToScroll > _maxVelocity)
-        {
-            _maxVelocity = forceToScroll;
-        }
-        
-        Debug.Log($"ScrollRect: {forceToScroll} MaxVelocity: {_maxVelocity} - normalize 0-1: {forceToScroll / _maxVelocity}");
-    }
+    
 }
