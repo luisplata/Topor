d6e6b5706638c826a3f34f49d4f139fb5cd2c94a
Luis Plata <www.luisplata@gmail.com>
2024-10-03 23:04:10 -0500

    MainMenuIntegrated with the final art

---
+++ Assets/Scripts/MainMenu/ActionCustom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ActionCustom : MonoBehaviour
+{
+    [SerializeField] private UnityEvent action;
+    
+    public void Execute()
+    {
+        action.Invoke();
+    }
+}
